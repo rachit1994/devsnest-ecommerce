8acf88bb1fa41932c1c9dae4ec853b8e97d1675c
Rachit Srivastava <rachitsrivastava@Rachits-Mac-mini.local>
2021-10-12 14:38:16 +0530

    day-22

---
+++ documentationJsdoc.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @param {string} name
+ * @param {string} email
+ * @param {Number} number
+ * @returns {Object}
+ */
+function docTest(name, email, number) {
+  return {
+    name,
+    email,
+    number,
+  };
+}
+
+/**
+ *
+ * @param {string} name
+ * @param {string} email
+ * @param {Number} number
+ */
+class Person {
+  constructor(name, email, number) {}
+}
+
